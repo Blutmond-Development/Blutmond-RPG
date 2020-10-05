998e91d8055e70523757b2ddbaad4014bce044b7
Charismara <craoulis@gmail.com>
2020-10-05 15:36:01 +0200

    add class icon and class parent to CharacterClass it is now possible to add an icon and a parent class to a CharacterClass
    Signed-off-by: Charismara <craoulis@gmail.com>

--- src/main/java/de/blutmondgilde/blutmondrpg/characterclass/CharacterClass.java
+++ src/main/java/de/blutmondgilde/blutmondrpg/characterclass/CharacterClass.java
@@ -1,12 +1,39 @@
 package de.blutmondgilde.blutmondrpg.characterclass;
 
+import com.feed_the_beast.mods.ftbguilibrary.icon.Icon;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.registries.ForgeRegistryEntry;
+import java.util.Optional;
 
 public abstract class CharacterClass extends ForgeRegistryEntry<CharacterClass> implements ICharacterClass {
+    final Icon icon;
+    final Optional<CharacterClass> parent;
+
+    public CharacterClass() {
+        this(null);
+    }
+
+    public CharacterClass(Icon icon) {
+        this.icon   = icon;
+        this.parent = Optional.empty();
+    }
+
+    public CharacterClass(Icon icon, Optional<CharacterClass> parent) {
+        this.icon   = icon;
+        this.parent = parent;
+    }
+
     @Override
     public ITextComponent getDisplayName() {
         return new TranslationTextComponent(getRegistryName().getNamespace() + "." + getRegistryName().getPath() + ".name");
+    }
+
+    public Icon getIcon() {
+        return icon;
+    }
+
+    public Optional<CharacterClass> getParent() {
+        return parent;
     }
 }

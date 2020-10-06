8a314efe56c6461fa83ad17652c288515725a417
Charismara <craoulis@gmail.com>
2020-10-06 21:08:01 +0200

    add a few basic character classes added MageCharacterClass, PriestCharacterClass, WarriorCharacterClass, ScoutCharacterClass
    Signed-off-by: Charismara <craoulis@gmail.com>

---
+++ src/main/java/de/blutmondgilde/blutmondrpg/characterclass/classes/MageCharacterClass.java
@@ -0,0 +1,58 @@
+package de.blutmondgilde.blutmondrpg.characterclass.classes;
+
+import com.feed_the_beast.mods.ftbguilibrary.icon.Icon;
+import de.blutmondgilde.blutmondrpg.characterclass.CharacterClass;
+import de.blutmondgilde.blutmondrpg.util.Constants;
+import net.minecraft.util.ResourceLocation;
+import java.util.Optional;
+
+public class MageCharacterClass extends CharacterClass {
+    public MageCharacterClass() {
+        super(Icon.getIcon(new ResourceLocation(Constants.MOD_ID, "textures/icons/mage.png")), Optional.empty());
+    }
+
+    @Override
+    public double calculateHPModifier(int level) {
+        return 10 + level;
+    }
+
+    @Override
+    public double calculateKnockbackResistanceModifier(int level) {
+        return 0;
+    }
+
+    @Override
+    public float calculateMovementSpeedModifier(int level) {
+        return 0.7F + level * 0.001F;
+    }
+
+    @Override
+    public double calculateDamageModifier(int level) {
+        return 0.5D;
+    }
+
+    @Override
+    public double calculateKnockbackModifier(int level) {
+        return 0;
+    }
+
+    @Override
+    public double calculateAttackSpeedModifier(int level) {
+        return 4.0D;
+    }
+
+    @Override
+    public double calculateArmorModifier(int level) {
+        return 0.0D;
+    }
+
+    @Override
+    public double calculateArmorToughnessModifier(int level) {
+        return 0.0D;
+    }
+
+    @Override
+    public double calculateMaxMana(int level) {
+        return 20 + level * 5;
+    }
+}
---
+++ src/main/java/de/blutmondgilde/blutmondrpg/characterclass/classes/ScoutCharacterClass.java
@@ -0,0 +1,58 @@
+package de.blutmondgilde.blutmondrpg.characterclass.classes;
+
+import com.feed_the_beast.mods.ftbguilibrary.icon.Icon;
+import de.blutmondgilde.blutmondrpg.characterclass.CharacterClass;
+import de.blutmondgilde.blutmondrpg.util.Constants;
+import net.minecraft.util.ResourceLocation;
+import java.util.Optional;
+
+public class ScoutCharacterClass extends CharacterClass {
+    public ScoutCharacterClass() {
+        super(Icon.getIcon(new ResourceLocation(Constants.MOD_ID, "textures/icons/scout.png")), Optional.empty());
+    }
+
+    @Override
+    public double calculateHPModifier(int level) {
+        return 15 + level * 2;
+    }
+
+    @Override
+    public double calculateKnockbackResistanceModifier(int level) {
+        return 0;
+    }
+
+    @Override
+    public float calculateMovementSpeedModifier(int level) {
+        return 0.8F + level * 0.01F;
+    }
+
+    @Override
+    public double calculateDamageModifier(int level) {
+        return 2.0D;
+    }
+
+    @Override
+    public double calculateKnockbackModifier(int level) {
+        return 0;
+    }
+
+    @Override
+    public double calculateAttackSpeedModifier(int level) {
+        return 6.0D;
+    }
+
+    @Override
+    public double calculateArmorModifier(int level) {
+        return 2.0D;
+    }
+
+    @Override
+    public double calculateArmorToughnessModifier(int level) {
+        return 2.0D;
+    }
+
+    @Override
+    public double calculateMaxMana(int level) {
+        return 10 + level * 2;
+    }
+}

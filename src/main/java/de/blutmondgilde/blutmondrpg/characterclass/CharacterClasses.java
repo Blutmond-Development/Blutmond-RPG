769cad0a0b8cfb1d4d53b0be70c9e2f6c6d7606d
Charismara <craoulis@gmail.com>
2020-10-06 21:10:54 +0200

    register new basic character classes
    Signed-off-by: Charismara <craoulis@gmail.com>

--- src/main/java/de/blutmondgilde/blutmondrpg/characterclass/CharacterClasses.java
+++ src/main/java/de/blutmondgilde/blutmondrpg/characterclass/CharacterClasses.java
@@ -2,6 +2,10 @@ package de.blutmondgilde.blutmondrpg.characterclass;
 
 import de.blutmondgilde.blutmondrpg.BlutmondRPG;
 import de.blutmondgilde.blutmondrpg.characterclass.classes.DefaultCharacterClass;
+import de.blutmondgilde.blutmondrpg.characterclass.classes.MageCharacterClass;
+import de.blutmondgilde.blutmondrpg.characterclass.classes.PriestCharacterClass;
+import de.blutmondgilde.blutmondrpg.characterclass.classes.ScoutCharacterClass;
+import de.blutmondgilde.blutmondrpg.characterclass.classes.WarriorCharacterClass;
 import de.blutmondgilde.blutmondrpg.util.Constants;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
@@ -12,6 +16,10 @@ import org.apache.logging.log4j.LogManager;
 public class CharacterClasses {
     private static final DeferredRegister<CharacterClass> CHARACTER_CLASS_REGISTRY = DeferredRegister.create(CharacterClass.class, Constants.MOD_ID);
     public static final RegistryObject<CharacterClass> DEFAULT_CLASS = CHARACTER_CLASS_REGISTRY.register("none", DefaultCharacterClass::new);
+    public static final RegistryObject<CharacterClass> MAGE_CLASS = CHARACTER_CLASS_REGISTRY.register("mage", MageCharacterClass::new);
+    public static final RegistryObject<CharacterClass> PRIEST_CLASS = CHARACTER_CLASS_REGISTRY.register("priest", PriestCharacterClass::new);
+    public static final RegistryObject<CharacterClass> SCOUT_CLASS = CHARACTER_CLASS_REGISTRY.register("scout", ScoutCharacterClass::new);
+    public static final RegistryObject<CharacterClass> WARRIOR_CLASS = CHARACTER_CLASS_REGISTRY.register("warrior", WarriorCharacterClass::new);
 
     /**
      * Called from {@link BlutmondRPG#BlutmondRPG()}.
